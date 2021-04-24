5df08ba579f47ae169a5e3cba705b686942c5943
Justyna Zarzycka <justyna.zarzycka70@gmail.com>
2021-04-24 10:53:31 +0200

    Initial Commit

---
+++ GitHubRepos/GitHubReposApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubReposApp.swift
+//  GitHubRepos
+//
+//  Created by Justyna Zarzycka on 24/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct GitHubReposApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
